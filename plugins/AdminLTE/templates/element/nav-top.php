cff2ebad44917c5250090ae89dcb8e5609f7ebbc
Gabriel Antunes Rabello <142711403+Rabellog@users.noreply.github.com>
2024-09-20 10:28:41 -0300

    Update nav-top.php

--- plugins/AdminLTE/templates/element/nav-top.php
+++ plugins/AdminLTE/templates/element/nav-top.php
@@ -2,8 +2,8 @@
     <ul>
         <li><a href="#">Ver Catálogo</a></li>
         <li><a href="#">Pesquisa por carro</a></li>
-        <li><a href="#">Suporte</a></li>
-        <li><a href="https://wa.me/5548996186913?text=Olá%2C%20preciso%20de%20suporte." target="_blank" id="adicionarCarros">Adicionar carro</a></li>
+        <li><a href="https://wa.me/5548996186913?text=Olá%2C%20preciso%20de%20suporte." target="_blank">Suporte</a></li>
+        <li><a href="#" id="adicionarCarros">Adicionar carro</a></li>
         <li><a href="#">Adicionar peça</a></li>
     </ul>
     <button class="login">Acesso Administrativo</button>
