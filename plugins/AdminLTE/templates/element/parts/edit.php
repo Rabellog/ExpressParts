5a8a237069514ede3086c9d9d255ca3a037ef666
GABRIEL ANTUNES RABELLO <gabriel.rabello@pmc.local>
2024-10-08 10:28:44 -0300

    front do editar peças

--- plugins/AdminLTE/templates/element/parts/edit.php
+++ plugins/AdminLTE/templates/element/parts/edit.php
@@ -1,52 +1,43 @@
-<?php echo $this->Html->css('parts/edit-pecas.css'); ?>
+<?php echo $this->Html->css('parts/edit-parts.css'); ?>
 
-<?php echo $this->element('cars/add') ?>
+<?php echo $this->element('parts/add') ?>
 
 
 <div class="modal fade" id="modalEditarPecas" tabindex="-1" role="dialog" aria-labelledby="modalAdicionarLabel" aria-hidden="true">
   <div class="modal-dialog">
-    <div class="modal-body-cars">
+    <div class="modal-body-parts">
 
-      <div class="top-modal-cars">
+      <div class="top-modal-parts">
         <h1>Editar peças</h1>
         <button class="fechar" type="button" data-dismiss="modal" aria-label="Close">
           <i class="fa-solid fa-xmark"></i>
         </button>
       </div>
 
-      <?php echo $this->Form->create(null, ['role' => 'form', 'url' => ['controller' => 'Cars', 'action' => 'add']]); ?>
-
-      <div class="modal-body-form-cars">
-        <div class="form-carros">
-
-          <label>
-            <span>Nome da peça:</span>
-            <div class="box-modal-input">
-              <input type="text" name="name" id="nameEditCar">
-            </div>
-          </label>
-
-          <div class="edit-view-cars">
-            <ul class="edit-results-cars">
-              <li class="edit-result-car">
-                <p>Teste</p>
-                <div class="xesque">
-                <i class="fa-solid fa-trash"></i>
-                <span type="button" id="aadicionarPecas">
-                  <i class="fa-solid fa-pen"></i>
-                </span>
-                </div>
-              </li>
-            </ul>
-          </div>x
+      <div class="partsSearchEdit">
+        <label>
+          <span>Nome dos veículos:</span>
+          <div class="box-modal-input searchs">
+            <input type="text" id="namePartsSearchEdit">
+          </div>
+        </label>
+        
+        <ul class="parts-viewEdit">
+        <li>
+          <input type="hidden" value="teste" name="teste">
+          <span>teste</span>
+          <div><i class="fa-solid fa-trash"></i>
+          <span type="button" id="aadicionarPecass">
+            <i class="fa-solid fa-pen"></i>
+          </span>
         </div>
-      </div>
+      </li>
+        </ul>
 
-      <?php echo $this->Form->end(); ?>
+      </div>
 
     </div>
   </div>
 </div>
 <?php $this->start('scriptBottom'); ?>
-<?php echo $this->Html->script('cars/add.js'); ?>
 <?php $this->end(); ?>
