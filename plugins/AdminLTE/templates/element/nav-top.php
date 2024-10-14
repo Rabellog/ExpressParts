5932be92ceedaa94908f9b1c124e51f858d2167e
GABRIEL ANTUNES RABELLO <gabriel.rabello@pmc.local>
2024-10-14 11:13:16 -0300

    teste

--- plugins/AdminLTE/templates/element/nav-top.php
+++ plugins/AdminLTE/templates/element/nav-top.php
@@ -6,8 +6,7 @@
         <li><a href="https://wa.me/5548996186913?text=Olá%2C%20preciso%20de%20suporte." target="_blank">Suporte</a></li>
         <li><a href="#" id="adicionarCarros">Adicionar carro</a></li>
         <li><a href="#" id="adicionarPecas">Adicionar peça</a></li>
-        <li><a href="#" id="editarCarros">Editar carros</a></li>
-        <li><a href="#" id="editarPecas">Editar peças</a></li>
+        <li><a href="#" onclick="window.location.href='cars-list'" id="editarCarros">Editar carros</a><
     </ul>
     <button class="login" onclick="window.location.href='login'">Acesso Administrativo</button>
     <button class="logout" onclick="window.location.href='users/logout'">Logout</button>
@@ -16,4 +15,3 @@
     const usuarioExiste = <?= $usuarioExiste ?>;
 </script>
 <?php echo $this->Html->css('nav.css'); ?>
-
---
+++ plugins/AdminLTE/templates/layout/cars-list.php
@@ -0,0 +1,28 @@
+<?php echo $this->Html->css('teste.css'); ?>
+
+<main>
+    <label class="xesq">
+        <input type="text">
+        <div class="c">
+            <i class="fa-solid fa-magnifying-glass"></i>          
+        </div>
+    </label>
+    <ul>
+        <li>
+            <span>
+                Testando
+            </span>
+            <p>a</p>
+            <p>b</p>
+            <p>c</p>
+            <div class="buttons">
+                <div class="a">
+                <i class="fa-solid fa-trash"></i>
+            </div>
+            <div class="b">
+                <i class="fa-solid fa-file-pen"></i>
+            </div>
+        </div>
+        </li>
+    </ul>
+  </main>  
